7e8359515eea6552ca77ccdad6b9827684e08535
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-29 00:56:17 +0600

    added functionality of call button

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ hearts.forEach(function(heart){
         heartCountElement.innerText=heartCount;
     })
 })
+
+
+// Call Part
+
+const coinElement = document.getElementById("coin-count");
+let coins = parseInt(coinElement.innerText);
+const callButtons = document.querySelectorAll(".call-btn");
+const callHistory = document.getElementById("call-history");
+
+callButtons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const card = btn.closest(".flex.flex-col");
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNumber = card.querySelector(".service-number").innerText;
+
+     if (coins < 20) {
+      alert("Not enough coins! You need at least 20 coins to make a call.");
+      return; 
+    }
+    alert(`Calling ${serviceName} at ${serviceNumber}`);
+
+     coins -= 20;
+    coinElement.innerText = coins;
+    const li = document.createElement("li");
+    li.innerText = `${serviceName} - ${serviceNumber}`;
+    callHistory.appendChild(li);
+  });
+});
